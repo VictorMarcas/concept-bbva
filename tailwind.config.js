9ca9fe29e0e08b4ea0a45ff9f9701d2e7e17055d
Victor Marcas <victor11687@gmail.com>
2022-05-06 04:40:15 -0500

    HTML y assets concept BBVA APP

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    content: ["./*.html"],
+    theme: {
+        extend: {
+            fontFamily: {
+                neue: ['"Neue"', 'sans-serif'],
+                'pro-display': ['"Pro Display"', 'sans-serif'],
+            },
+            colors: {
+                'dark-blue': '#072146',
+                'turquoise': '#2DCDCD',
+                'blue': '#1873B9',
+                'white': '#FFFFFF',
+                'gray': '#60708F',
+                'black': '#121212',
+            }
+        },
+    },
+    plugins: [],
+}
